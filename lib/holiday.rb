d068d77a0615f5b71500f4915b7d766ce0332f1f
Antonio James <asjames18@gmail.com>
2020-07-17 01:36:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
 holiday_supplies[:spring][:memorial_day] 
-holiday_supplies[:spring][:memorial_day].flatten
+holiday_supplies[:spring][:memorial_day].flatten.min
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
